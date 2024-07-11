fe1e432c9538b75f078298fb2fefa361a06cf948
Ali Abbasi <al5690410@gmail.com>
2024-07-11 21:28:32 +0500

    feat: forgot page

--- frontend/app/forgotpassword/components/form.tsx
+++ frontend/app/forgotpassword/components/form.tsx
@@ -11,44 +11,36 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-export default function LoginForm() {
+export default function ForgotPasswordForm() {
   return (
-    <div className="h-screen w-full flex justify-center items-center">
+    <div className="h-screen w-full flex justify-center items-center space-x-4">
       <Card className="mx-auto max-w-sm">
         <CardHeader>
-          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardTitle className="text-2xl">Forgot Password</CardTitle>
           <CardDescription>
-            Enter your email below to login to your account
+            Enter your email below, A verification link will be sent to your
           </CardDescription>
         </CardHeader>
         <CardContent>
           <div className="grid gap-4">
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
+
               <Input
                 id="email"
                 type="email"
                 placeholder="m@example.com"
                 required
               />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <Link
-                  href="/forgotpassword"
-                  className="ml-auto inline-block text-sm underline"
-                >
-                  Forgot your password?
-                </Link>
-              </div>
-              <Input id="password" type="password" required />
+              <Link
+                href="/login"
+                className="ml-auto inline-block text-sm underline"
+              >
+                login
+              </Link>
             </div>
             <Button type="submit" className="w-full">
-              Login
-            </Button>
-            <Button variant="outline" className="w-full">
-              Login with Google
+              Submit
             </Button>
           </div>
           <div className="mt-4 text-center text-sm">
@@ -58,6 +50,15 @@ export default function LoginForm() {
             </Link>
           </div>
         </CardContent>
+      </Card>
+      <Card className=" max-w-[350px]">
+        <CardHeader>
+          <CardTitle className="text-2xl">Email Sent</CardTitle>
+          <CardDescription>
+            A verification link has been sent to your email. Please check your
+            email to reset your password.
+          </CardDescription>
+        </CardHeader>
       </Card>
     </div>
   );
