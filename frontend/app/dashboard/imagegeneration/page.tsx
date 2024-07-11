827a6d4415f0ee47b4a4f4fb7cfe4ce5ac72885b
Ali Abbasi <al5690410@gmail.com>
2024-07-11 21:51:35 +0500

    fix: building issues with vercel

---
+++ frontend/app/dashboard/imagegeneration/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ImageGenerationPage = () => {
+  return <div></div>;
+};
+
+export default ImageGenerationPage;
---
+++ frontend/app/dashboard/rewrite/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RewritePage = () => {
+  return <div></div>;
+};
+
+export default RewritePage;
