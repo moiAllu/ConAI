1086e23fa0b5f6dd57c48a39ec717e93f4b96c1a
Ali Abbasi <al5690410@gmail.com>
2024-09-05 19:44:32 +0500

    feat: refactoring

--- frontend/lib/apicalls/auth.ts
+++ frontend/lib/apicalls/auth.ts
@@ -1,4 +1,4 @@
-// "use client";
+"use client";
 // export default function auth(url:string,data:any,method:string){
 //     return fetch(`http://localhost:8000/api${url}`,{
 //         method:data? method:"GET",
@@ -11,3 +11,54 @@
 //         return res.json();
 //     })
 // }
+interface promptMods {
+    inputFormat: string,
+    inputType : string,
+    inputTone : string,
+    inputAgeGroup : string,
+    inputLength : string, 
+  };
+
+    export const generateAiResponse = async (promptMods:promptMods,prompt:string,userId:string,chatId:string) => {
+        return fetch(`http://localhost:8000/api/ai-writing`,{
+            method:"POST",
+            credentials:"include",
+            headers:{
+                "Content-Type":"application/json",
+                Authorization: localStorage.getItem("accessToken") || "",
+            },
+            body: JSON.stringify({promptMods,prompt,userId,chatId}),
+        }).then(res=>{
+            return res.json();
+        })
+    }
+    export const getUserAiWritings = async (userId:string) => {
+        return fetch(`http://localhost:8000/api/ai-writing/writings/${userId}`,{
+            method:"GET",
+            credentials:"include",
+        
+            headers:{
+                "Content-Type":"application/json",
+                Authorization: localStorage.getItem("accessToken") || "",
+            
+            },
+        }).then(res=>{
+            return res.json();
+        }
+        )
+    }
+    export const  getAiWritingById = async (id:string,userId:string) => {
+        return fetch(`http://localhost:8000/api/ai-writing/${id}/${userId}`,{
+            method:"GET",
+            credentials:"include",
+            headers:{
+                "Content-Type":"application/json",
+                Authorization: localStorage.getItem("accessToken") || "",
+            },
+        }).then(res=>{
+            return res.json();
+        }
+        )
+    }
+
+
