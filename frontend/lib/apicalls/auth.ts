e761b9a22addb1ddc3e2cb1911812adc9220a718
Ali Abbasi <al5690410@gmail.com>
2024-10-15 02:27:44 +0500

    feat: api calls for content detection

--- frontend/lib/apicalls/auth.ts
+++ frontend/lib/apicalls/auth.ts
@@ -60,5 +60,46 @@ interface promptMods {
         }
         )
     }
+    export  const getUserContentDetections = async (userId:string) => {
+        return fetch(`http://localhost:8000/api/content-detection/detections/${userId}`,{
+            method:"GET",
+            credentials:"include",
+            headers:{
+                "Content-Type":"application/json",
+                Authorization: localStorage.getItem("accessToken") || "",
+            },
+        }).then(res=>{
+            return res.json();
+        }
+        )
+    }
+    export const getContentDetectionById = async (userId:string, id:string) => {
+        return fetch(`http://localhost:8000/api/content-detection/detection/${userId}/${id}`,{
+            method:"GET",
+            credentials:"include",
+            headers:{
+                "Content-Type":"application/json",
+                Authorization: localStorage.getItem("accessToken") || "",
+            },
+        }).then(res=>{
+            return res.json();
+        }
+        )
+    }
+    export const createContentDetection = async (userId:string, method:string, content:string, compareTo?:string) => {
+        return fetch(`http://localhost:8000/api/content-detection/detection`,{
+            method:"POST",
+            credentials:"include",
+            headers:{
+                "Content-Type":"application/json",
+                Authorization: localStorage.getItem("accessToken") || "",
+            },
+            body: JSON.stringify({userId, method, content, compareTo}),
+        }).then(res=>{
+            return res.json();
+        }
+        )
+    }
+
 
 
