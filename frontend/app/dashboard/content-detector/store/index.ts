c618a9d3846824a43ae3cdde5cce49378e231e62
Ali Abbasi <al5690410@gmail.com>
2024-10-15 02:24:39 +0500

    test: add method variable for different responses

--- frontend/app/dashboard/content-detector/store/index.ts
+++ frontend/app/dashboard/content-detector/store/index.ts
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import { create } from 'zustand';
 
 interface IPlagrismDetection{
+    _id?: string;
     querywords: number;
     count: number;
     cost: number;
@@ -16,6 +17,7 @@ interface IPlagrismDetection{
     }[];
 }
 interface IAiDetection{
+    method?:string
     _id?: string;
     prompt: string;
     createdAt?: Date;
@@ -47,34 +49,112 @@ interface ICompareHistory {
 }
 
 interface States {
-    aiHistory: IAiDetectionHistory[];
-    plagrismHistory: IPlagrismDetectionHistory[];
-    compareHistory: ICompareHistory[];
+    aiHistory: IAiDetectionHistory;
+    plagrismHistory: IPlagrismDetectionHistory;
+    compareHistory: ICompareHistory;
 }
 
 interface Actions {
-    setAiHistory: (aiHistory: IAiDetectionHistory[]) => void;
-    setPlagrismHistory: (plagrismHistory: IPlagrismDetectionHistory[]) => void;
-    setCompareHistory: (compareHistory: ICompareHistory[]) => void;
-    addAiHistory: (aiHistory: IAiDetectionHistory) => void;
-    addPlagrismHistory: (plagrismHistory: IPlagrismDetectionHistory) => void;
-    addCompareHistory: (compareHistory: ICompareHistory) => void;
+    addAiHistory: (aiHistory: IAiDetection, userId:string, method:string) => void;
+    addPlagrismHistory: (plagrismHistory: IPlagrismDetection, userId:string, method:string) => void;
+    addCompareHistory: (compareHistory: ICompare, userId:string, method:string) => void;
     removeAiHistory: (userId: string) => void;
     removePlagrismHistory: (userId: string) => void;
     removeCompareHistory: (userId: string) => void;
 }
 
-export const useContentDetectorStore = create<States & Actions>((set) => ({
-    aiHistory: [],
-    plagrismHistory: [],
-    compareHistory: [],
-    setAiHistory: (aiHistory) => set({aiHistory}),
-    setPlagrismHistory: (plagrismHistory) => set({plagrismHistory}),
-    setCompareHistory: (compareHistory) => set({compareHistory}),
-    addAiHistory: (aiHistory) => set((state) => ({aiHistory: [...state.aiHistory, aiHistory]})),
-    addPlagrismHistory: (plagrismHistory) => set((state) => ({plagrismHistory: [...state.plagrismHistory, plagrismHistory]})),
-    addCompareHistory: (compareHistory) => set((state) => ({compareHistory: [...state.compareHistory, compareHistory]})),
-    removeAiHistory: (userId) => set((state) => ({aiHistory: state.aiHistory.filter((history) => history.userId !== userId)})),
-    removePlagrismHistory: (userId) => set((state) => ({plagrismHistory: state.plagrismHistory.filter((history) => history.userId !== userId)})),
-    removeCompareHistory: (userId) => set((state) => ({compareHistory: state.compareHistory.filter((history) => history.userId !== userId)})),
-}));
+export const useContentDetectorStore = create<States & Actions>((set) => (
+    {
+        aiHistory:{
+            method:'',
+            userId:'',
+            aiDetectionHistory: []
+        },
+        plagrismHistory:{
+            method:'',
+            userId:'',
+            plagrismDetectionHistory: []
+        },
+        compareHistory:{
+            method:'',
+            userId:'',
+            compareHistory: []
+        },
+        addAiHistory: (aiHistory, userId, method) => {
+            set((state) => {
+                const newState = _.cloneDeep(state);
+                const existing = _.find(newState.aiHistory.aiDetectionHistory, { _id: aiHistory._id });
+                if (existing) {
+                    return newState
+                } else {
+                    newState.aiHistory = {
+                        method,
+                        userId,
+                        aiDetectionHistory: [...newState.aiHistory.aiDetectionHistory, aiHistory]
+                    }
+                    return newState;
+                }
+            });
+        },
+        addPlagrismHistory: (plagrismHistory, userId, method) => {
+            set((state) => {
+                const newState = _.cloneDeep(state);
+                const existing = _.find(newState.plagrismHistory.plagrismDetectionHistory, { _id: plagrismHistory._id });
+                if (existing) {
+                    return newState
+                } else {newState.plagrismHistory = {
+                    method,
+                    userId,
+                    plagrismDetectionHistory: [...newState.plagrismHistory.plagrismDetectionHistory, plagrismHistory]
+                }
+                return newState;
+            }
+            });
+        },
+        addCompareHistory: (compareHistory, userId, method) => {
+            set((state) => {
+                const newState = _.cloneDeep(state);
+                newState.compareHistory = {
+                    method,
+                    userId,
+                    compareHistory: [...newState.compareHistory.compareHistory, compareHistory]
+                }
+                return newState;
+            });
+        },
+        removeAiHistory: (userId) => {
+            set((state) => {
+                const newState = _.cloneDeep(state);
+                newState.aiHistory = {
+                    method:'',
+                    userId:'',
+                    aiDetectionHistory: []
+                }
+                return newState;
+            });
+        },
+        removePlagrismHistory: (userId) => {
+            set((state) => {
+                const newState = _.cloneDeep(state);
+                newState.plagrismHistory = {
+                    method:'',
+                    userId:'',
+                    plagrismDetectionHistory: []
+                }
+                return newState;
+            });
+        },
+        removeCompareHistory: (userId) => {
+            set((state) => {
+                const newState = _.cloneDeep(state);
+                newState.compareHistory = {
+                    method:'',
+                    userId:'',
+                    compareHistory: []
+                }
+                return newState;
+            });
+        }
+        
+    }
+));
