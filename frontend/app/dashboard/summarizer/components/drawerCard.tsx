678c8751dfbe09dc58a856c41706509161b0ee97
Ali Abbasi <al5690410@gmail.com>
2024-11-07 06:02:01 +0500

    feat: zustand setup, fetch history, create new summarize

--- frontend/app/dashboard/summarizer/components/drawerCard.tsx
+++ frontend/app/dashboard/summarizer/components/drawerCard.tsx
@@ -13,13 +13,25 @@ import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 
 const DrawerCard = () => {
+  const [intensity, setIntensity] = React.useState("Medium");
+  const [content, setContent] = React.useState("");
+  const formSubmitHandler = async (e: any) => {
+    console.log(intensity, content);
+    e.preventDefault();
+  };
   return (
-    <form className="flex flex-col h-full w-full items-start gap-6 overflow-auto max-w-md sm:p-0 p-2">
+    <form
+      className="flex flex-col h-full w-full items-start gap-6 overflow-auto max-w-md sm:p-0 p-2"
+      onSubmit={formSubmitHandler}
+    >
       <fieldset className="flex flex-col rounded-lg border p-4 w-full  h-full rela">
         <legend className="px-1 text-sm font-medium">Settings</legend>
         <div className="w-full ">
-          <Label htmlFor="role">Level</Label>
-          <Select defaultValue="Medium">
+          <Label htmlFor="role">Intensity</Label>
+          <Select
+            defaultValue="Medium"
+            onValueChange={(e: any) => setIntensity(e.target.value)}
+          >
             <SelectTrigger>
               <SelectValue placeholder="Select a role" />
             </SelectTrigger>
@@ -30,9 +42,11 @@ const DrawerCard = () => {
             </SelectContent>
           </Select>
         </div>
-        <div className="w-full h-full">
+        <div className="w-full h-full py-3 flex flex-col gap-2">
           <Label htmlFor="content">Content</Label>
           <Textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
             id="content"
             placeholder="You are a..."
             className="sm:h-[95%] resize-none relative border-0 bg-muted/50 p-1 shadow-none focus-visible:ring-0"
