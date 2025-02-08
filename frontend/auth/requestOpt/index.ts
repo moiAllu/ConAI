d2e6635ac0fb663a8337cf345bfdca871db506f0
Ali Abbasi <al5690410@gmail.com>
2025-02-08 09:35:01 +0500

    feat: opt request endpoint in nextjs server

---
+++ frontend/auth/requestOpt/index.ts
@@ -0,0 +1,53 @@
+import { UserModel } from "@/lib/mongodb/model/userModel";
+import { OtpVerification } from "@/lib/mongodb/model/otpVerification"
+import { sendVerificationCode } from "@/lib/helper/generatePasswordResetLink";
+
+export const requestOtp = async (email:string) => {
+    try {
+      // Check if user exists
+      const otp = Math.floor(100000 + Math.random() * 900000).toString();
+      if(!email ){
+        return {
+          status:400,     
+          message: "Please enter email",
+      }
+      }
+      let user = await UserModel.findOne({
+        email: email,
+      });
+      if (!user) {
+        return {
+            status:400,     
+            message: "User not found",
+        }
+      }
+        if(user.verified){
+            return {
+                status: 300,
+                message: "User already verified"
+            };
+        }
+       const alreadyOtpGen= await OtpVerification.findOneAndUpdate({email},{
+            otp
+        });
+        if(!alreadyOtpGen){
+         const otpAgainstEmail = new OtpVerification({email, otp})
+         await otpAgainstEmail.save();
+         if (!otpAgainstEmail) {
+            return {
+                status:400,
+                message: "OTP generation failed"
+            };
+        }
+        }
+        const verificationCodeToMail = await sendVerificationCode(email, otp) as any;
+        return verificationCodeToMail;
+      } catch (e) {
+      console.log(e);
+      const response = {
+        status: 500,
+        message: e,
+        }
+    return response;
+    }
+}
