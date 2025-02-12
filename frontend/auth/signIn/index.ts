fddfe614481a87734185e862319b521a0a0b8ce4
Ali Abbasi <al5690410@gmail.com>
2025-02-12 06:59:32 +0500

    fix: signin bug

--- frontend/auth/signIn/index.ts
+++ frontend/auth/signIn/index.ts
@@ -3,61 +3,69 @@ import { serializeCookie } from "@/lib/helper/serialize";
 import { tokenization } from "@/lib/helper/token";
 import { UserModel } from "@/lib/mongodb/model/userModel";
 interface SignInInterface {
-        email:string,
-        password: string
+  email: string;
+  password: string;
 }
-export const signIn = async ( procedure: string, { email, password }: SignInInterface ) => {
-    try {
-      // Check if user exists
-      if(!email ){
-        return {
-          status:400,     
-          message: "Please enter email",
-      }
-      }
-      if(!password){
-        return {
-          status:400,     
-          message: "Password cannot be empty",
-      }
-      }
-      let user = await UserModel.findOne({
-        email: email,
+export const signIn = async (
+  procedure: string,
+  { email, password }: SignInInterface
+) => {
+  try {
+    // Check if user exists
+    if (!email) {
+      return {
+        status: 400,
+        message: "Please enter email",
+      };
+    }
+    if (!password) {
+      return {
+        status: 400,
+        message: "Password cannot be empty",
+      };
+    }
+    let user = await UserModel.findOne({
+      email: email,
+    });
+    if (!user) {
+      return {
+        status: 400,
+        message: "User not found",
+      };
+    }
+    if (CompareHash({ compare: password, comparedTo: user.password })) {
+      user.password = "";
+      const token = await tokenization({ user });
+      await serializeCookie({
+        name: "CONAI",
+        token,
+        properties: {
+          httpOnly: true,
+          maxAge: 8 * 60 * 60,
+          path: "/",
+          sameSite: false,
+          secure: false,
+          overwrite: true,
+        },
       });
-      if (!user) {
-        return {
-            status:400,     
-            message: "User not found",
-        }
-      }
-      // Check if password is correct
-      // const checkPass= utils.compareHash(user.password, password);
-      if(CompareHash({compare:password,comparedTo:user.password})){
-        user.password = "";
-        const token = await tokenization({user})
-        await serializeCookie({name:"CONAI",token,properties:{
-            httpOnly: true,
-                    maxAge: 8 * 60 * 60,
-                    path: "/",
-                    sameSite: false,
-                    secure: false,
-                    overwrite: true,
-        }})
-       const response = {
+      const response = {
         status: 200,
         message: "Login successful",
         user: user,
-        token:token,
-     }
-    return response;
-      }
-
-      } catch (e) {
-      console.log(e);
-      const response = {
-        status: 500,
-        message: e,
+        token: token,
+      };
+      return response;
     }
+    return {
+      status: 400,
+      message: "Incorrect password",
+    };
+  } catch (e) {
+    console.log(e);
+    const response = {
+      status: 500,
+      message: e,
+    };
     return response;
-    }
-}
+  }
+};
