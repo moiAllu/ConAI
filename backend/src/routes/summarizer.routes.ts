2d805a7fd953c9b8e83171ea1b2a06e67645180e
Ali Abbasi <al5690410@gmail.com>
2025-02-14 01:39:47 +0500

    feat: delete option for summarizer

--- backend/src/routes/summarizer.routes.ts
+++ backend/src/routes/summarizer.routes.ts
@@ -1,13 +1,30 @@
-import express from 'express';
-import { validateRoute } from '../helpers';
+import express from "express";
+import { validateRoute } from "../helpers";
 
-import {createSummarizer,getSummarizerByUserIdController, getSummarizerHistoryContorller,deleteSummarizerByIdController } from '../controllers';
+import {
+  createSummarizer,
+  getSummarizerByUserIdController,
+  getSummarizerHistoryContorller,
+  deleteSummarizerByIdController,
+} from "../controllers";
 
 const router = express.Router();
 
-router.post('/summarizer/create',validateRoute,createSummarizer );
-router.get('/summarizer/summarizes/:userId',validateRoute, getSummarizerHistoryContorller);
-router.get('/summarizer/:userId/:summarizeId',validateRoute, getSummarizerByUserIdController);
-router.delete('/summarizer/:userId/:summarizeId',validateRoute, deleteSummarizerByIdController);
+router.post("/summarizer/create", validateRoute, createSummarizer);
+router.get(
+  "/summarizer/summarizes/:userId",
+  validateRoute,
+  getSummarizerHistoryContorller
+);
+router.get(
+  "/summarizer/:userId/:summarizeId",
+  validateRoute,
+  getSummarizerByUserIdController
+);
+router.delete(
+  "/summarizer/:summarizeId/:userId",
+  validateRoute,
+  deleteSummarizerByIdController
+);
 
 export default router;
--- frontend/lib/apicalls/summarize.ts
+++ frontend/lib/apicalls/summarize.ts
@@ -1,13 +1,15 @@
 export const getUserSummarizesHistory = async (userId: string) => {
-
-  return fetch(`${process.env.NEXT_PUBLIC_CONAI_BACKEND_URL}/api/summarizer/summarizes/${userId}`, {
-    method: "GET",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: localStorage.getItem("accessToken") || "",
-    },
-  }).then((res) => {
+  return fetch(
+    `${process.env.NEXT_PUBLIC_CONAI_BACKEND_URL}/api/summarizer/summarizes/${userId}`,
+    {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("accessToken") || "",
+      },
+    }
+  ).then((res) => {
     return res.json();
   });
 };
@@ -34,19 +36,40 @@ export const createSummarize = async (
   content: string,
   userId: string
 ) => {
-  return fetch(`${process.env.NEXT_PUBLIC_CONAI_BACKEND_URL}/api/summarizer/create`, {
-    method: "POST",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: localStorage.getItem("accessToken") || "",
-    },
-    body: JSON.stringify({
-      intensity,
-      content,
-      userId,
-    }),
-  }).then((res) => {
+  return fetch(
+    `${process.env.NEXT_PUBLIC_CONAI_BACKEND_URL}/api/summarizer/create`,
+    {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("accessToken") || "",
+      },
+      body: JSON.stringify({
+        intensity,
+        content,
+        userId,
+      }),
+    }
+  ).then((res) => {
+    return res.json();
+  });
+};
+export const deleteSummarizeById = async (
+  summarizeId: string,
+  userId: string
+) => {
+  return fetch(
+    `${process.env.NEXT_PUBLIC_CONAI_BACKEND_URL}/api/summarizer/${summarizeId}/${userId}`,
+    {
+      method: "DELETE",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("accessToken") || "",
+      },
+    }
+  ).then((res) => {
     return res.json();
   });
 };
